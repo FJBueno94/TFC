42503fbe672ed8d8d7fa290631b3d7d990677137
FJBueno94 <fernando_jordi@hotmail.com>
2022-10-04 14:37:22 -0300

    adiciona o requisito 22 TDD

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -25,4 +25,52 @@ describe('/MATCHES', () => {
       expect(chaiHttpResponse.body[0]).to.have.property('awayTeam');
     });
   });
+
+  describe('GETALL IN PROGRESS', () => {
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).get('/matches?inProgress=true');
+    });
+
+    it('Valida retorno de matches em andamento como sucesso', async () => {
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.an('array');
+      expect(chaiHttpResponse.body[0]).to.have.property('homeTeam');
+      expect(chaiHttpResponse.body[0]).to.have.property('awayTeam');
+    });
+  });
+
+  describe('GETALL NOT IN PROGRESS', () => {
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).get('/matches?inProgress=false');
+    });
+
+    it('Valida retorno de matches não em andamento como sucesso', async () => {
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.an('array');
+      expect(chaiHttpResponse.body[0]).to.have.property('homeTeam');
+      expect(chaiHttpResponse.body[0]).to.have.property('awayTeam');
+    });
+  });
+
+  describe('POST PARTIDA INPROGRESS', () => {
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).post('/matches').send({
+        homeTeam: 1,
+        awayTeam: 2,
+        homeTeamGoals: 0,
+        awayTeamGoals: 2,
+        inProgress: 1,
+      });
+    });
+
+    it('Valida retorno de partida em andamento como sucesso', async () => {
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.an('object');
+      expect(chaiHttpResponse.body).to.have.property('homeTeam');
+      expect(chaiHttpResponse.body).to.have.property('awayTeam');
+      expect(chaiHttpResponse.body).to.have.property('homeTeamGoals');
+      expect(chaiHttpResponse.body).to.have.property('awayTeamGoals');
+      expect(chaiHttpResponse.body).to.have.property('inProgress');
+    });
+  });
 });
