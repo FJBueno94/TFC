701ecf02f9179bca01f78cb61babc0ed47702bab
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-27 20:54:34 -0300

    alterações para postar em repo pessoal

--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -14,46 +14,70 @@ describe('/POST', () => {
   describe('/LOGIN', () => {
     let chaiHttpResponse: Response;
     
-    before(async () => {
-      Sinon
-      .stub(Users, 'findOne')
-      .resolves({} as Users)
-    });
+    // before(async () => {
+    //   Sinon
+    //   .stub(Users, 'findOne')
+    //   .resolves({
+    //     email: 'test@test.com',
+    //     password: '123456',
+    //   } as Users)
+    // });
 
-    after(() => {
+    afterEach(() => {
       (Users.findOne as Sinon.SinonStub).restore();
     });
 
     it('Valida login como sucesso', async () => {
+      Sinon
+      .stub(Users, 'findOne')
+      .resolves({
+        email: 'test@test.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+      } as Users)
+
       chaiHttpResponse = await chai.request(app).post('/login').send({
         email: 'test@test.com',
-        password: '123456',
+        password: 'secret_admin',
       });
       expect(chaiHttpResponse).to.have.status(200);
       expect(chaiHttpResponse.body).to.have.property('token');
     });
 
     it('valida login como credenciais invalidas', async () => {
+      Sinon
+      .stub(Users, 'findOne')
+      .throws('nao era para ser chamado')
+
       chaiHttpResponse = await chai.request(app).post('/login').send({
         email: 'test',
         password: '123',
       });
-
+      expect((Users.findOne as Sinon.SinonStub).notCalled).to.be.true;
       expect(chaiHttpResponse).to.have.status(400);
       expect(chaiHttpResponse).to.have.property('error');
     });
 
     it('valida login sem credenciais', async () => {
+      Sinon
+      .stub(Users, 'findOne')
+      .throws('nao era para ser chamado')
+
       chaiHttpResponse = await chai.request(app).post('/login').send({
         email: '',
         password: '',
       });
 
+      expect((Users.findOne as Sinon.SinonStub).notCalled).to.be.true;
       expect(chaiHttpResponse).to.have.status(400);
       expect(chaiHttpResponse).to.have.property('error');
     });
 
     it('valida login com credenciais não existentes', async () => {
+      Sinon
+      .stub(Users, 'findOne')
+      .resolves(null)
+
+      
       chaiHttpResponse = await chai.request(app).post('/login').send({
         email: 'teste@teste.com',
         password: '123456',
